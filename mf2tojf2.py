76d949aea6a46d0882c0482fa2ac18d2d481fe5d
Kevin Marks <kevinmarks@gmail.com>
2015-12-03 17:13:41 -0800

    recursive descent

--- mf2tojf2.py
+++ mf2tojf2.py
@@ -6,17 +6,31 @@
 
 import logging
 
+
+def flattenProperties(items):
+    if len(items) <1:
+        return {}
+    item = items[0]
+     
+    if type(item) is dict:
+        if item.has_key("type"):
+            props ={"type":item.get("type",["-"])[0].split("-")[1:][0]}
+            properties =  item.get("properties",{})
+            for prop in properties:
+                props[prop] = flattenProperties(properties[prop])
+            return props
+        elif item.has_key("value"):
+            return item["value"]
+        else:
+            return ''
+    else:
+        return item
+    
+
 def mf2tojf2(mf2):
     """I'm going to have to recurse here"""
     jf2={}
     items = mf2.get("items",[])
-    if len(items):
-        item = items[0]
-        type = item.get("type",["-"])[0].split("-")[1:][0]
-        if type: 
-            jf2["type"] = type
-        properties =  item.get("properties",{})
-        for prop in properties:
-            jf2[prop] = properties[prop][0] # need to recurse
+    jf2=flattenProperties(items)
     #print mf2, jf2
     return jf2
--- mf2tojf2test.py
+++ mf2tojf2test.py
@@ -18,6 +18,97 @@ class BasicCheck(unittest.TestCase):
             "name": "Homebrew Website Club notes 2015-06-17",
             "url": "http://kevinmarks.com/hwc2015-06-17.html"
             })
+    def test_checkHentryeContent(self):
+         self.assertEqual(mf2tojf2.mf2tojf2({"items":[{"type": ["h-entry"], "properties": 
+                {"content":[{"html":"<p>this is <b>content</b>","value":"this is content"}],
+                 "name": ["Homebrew Website Club"]}}]}), 
+            {
+            "type": "entry",
+            "name": "Homebrew Website Club",
+            "content": "this is content"
+            })
+    def test_checkHentryTextAuthor(self):
+         self.assertEqual(mf2tojf2.mf2tojf2({"items":[{"type": ["h-entry"], "properties": 
+                {"author":["fred bloggs"],
+                 "name": ["Homebrew Website Club"]}}]}), 
+            {
+            "type": "entry",
+            "name": "Homebrew Website Club",
+            "author":"fred bloggs",
+            })
+    def test_checkHentryNestedAuthor(self):
+         self.assertEqual(mf2tojf2.mf2tojf2({"items":[{"type": ["h-entry"], "properties": 
+                {"author":[{"type":["h-card"],"properties": {"name":["fred bloggs"]}}],
+                 "name": ["Homebrew Website Club"]}}]}), 
+            {
+            "type": "entry",
+            "name": "Homebrew Website Club",
+            "author": {"type":"card",
+                        "name":"fred bloggs",}
+            })
+
+
+class SpeckCheck(unittest.TestCase):
+    def test_checkExample(self):
+        self.assertEqual(mf2tojf2.mf2tojf2(
+            {
+              "items": [
+                {
+                  "type": [
+                    "h-entry"
+                  ],
+                  "properties": {
+                    "author": [
+                      {
+                        "type": [
+                          "h-card"
+                        ],
+                        "properties": {
+                          "name": [
+                            "A. Developer"
+                          ],
+                          "url": [
+                            "http:\/\/example.com\/"
+                          ]
+                        },
+                        "value": "A. Developer"
+                      }
+                    ],
+                    "name": [
+                      "Hello World"
+                    ],
+                    "summary": [
+                      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus imperdiet ultrices pulvinar."
+                    ],
+                    "url": [
+                      "http:\/\/example.com\/2015\/10\/21"
+                    ],
+                    "published": [
+                      "2015-10-21T12:00:00-0700"
+                    ],
+                    "content": [
+                      {
+                        "html": "<p>Donec dapibus enim lacus, <i>a vehicula magna bibendum non</i>. Phasellus id lacinia felis, vitae pellentesque enim. Sed at quam dui. Suspendisse accumsan, est id pulvinar consequat, urna ex tincidunt enim, nec sodales lectus nulla et augue. Cras venenatis vehicula molestie. Donec sagittis elit orci, sit amet egestas ex pharetra in.</p>",
+                        "value": "Donec dapibus enim lacus, a vehicula magna bibendum non. Phasellus id lacinia felis, vitae pellentesque enim. Sed at quam dui. Suspendisse accumsan, est id pulvinar consequat, urna ex tincidunt enim, nec sodales lectus nulla et augue. Cras venenatis vehicula molestie. Donec sagittis elit orci, sit amet egestas ex pharetra in."
+                      }
+                    ]
+                  }
+                }
+              ]
+            }), 
+            {
+              "type": "entry",
+              "author": {
+                "type": "card",
+                "url": "http://example.com",
+                "name": "A. Developer"
+              },
+              "url": "http://example.com/2015/10/21",
+              "published": "2015-10-21T12:00:00-0700",
+              "name": "Hello World",
+              "summary": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus imperdiet ultrices pulvinar.",
+              "content": "Donec dapibus enim lacus, a vehicula magna bibendum non. Phasellus id lacinia felis, vitae pellentesque enim. Sed at quam dui. Suspendisse accumsan, est id pulvinar consequat, urna ex tincidunt enim, nec sodales lectus nulla et augue. Cras venenatis vehicula molestie. Donec sagittis elit orci, sit amet egestas ex pharetra in."
+            })
 
 if __name__ == '__main__':
     unittest.main()
